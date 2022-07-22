93fd5d92ca40da4f2451c842c4cd2286e56ef717
cloudcodenyc <98148819+cloudcodenyc@users.noreply.github.com>
2022-07-22 01:39:39 -0400

    Update autoiso-macos-10.11.sh

--- autoiso-macos-10.11.sh
+++ autoiso-macos-10.11.sh
@@ -43,11 +43,11 @@ fi
 [ -e "./Install-OSX-$VERSION/" ] && rm -rf "./Install-OSX-$VERSION/"
 	hdiutil attach "./$ESD_NAME.dmg" -noverify -nobrowse -mountpoint "/Volumes/OSX-$VERSION-InstallESD/"
 	# Get InstallESD.dmg Total Non-Empty Size ( Bytes )
-	ESDSIZE=$(hdiutil imageinfo ./"$ESD_NAME".dmg | grep --line-buffered -e 'Total Non-Empty Bytes:' | cut -d ' ' -f4)
-	EFISIZE=$(hdiutil imageinfo ./"$ESD_NAME".dmg | grep --line-buffered -e 'partition-name: EFI System Partition' -A7 | grep -e 'partition-length:' | cut -d ' ' -f2)
+	ESDSIZE=$(hdiutil imageinfo "./$ESD_NAME.dmg" | grep --line-buffered -e 'Total Non-Empty Bytes:' | cut -d ' ' -f4)
+	EFISIZE=$(hdiutil imageinfo "./$ESD_NAME.dmg" | grep --line-buffered -e 'partition-name: EFI System Partition' -A7 | grep -e 'partition-length:' | cut -d ' ' -f2)
 	echo '' ; echo "$ESD_NAME.dmg" && echo "Filesize (bytes): $COUNT" ; echo ''
 
-[ ! -f "$RELEASE-$VERSION.dmg" ] && hdiutil create -o ./"$RELEASE-$VERSION".dmg -size "735	0m" -volname "$RELEASE-$VERSION" -layout SPUD -fs HFS+J
+[ ! -f "$RELEASE-$VERSION.dmg" ] && hdiutil create -o ./"$RELEASE-$VERSION".dmg -size "7350m" -volname "$RELEASE-$VERSION" -layout SPUD -fs HFS+J
 	set -x && hdiutil attach ./"$RELEASE-$VERSION".dmg -noverify -mountpoint /Volumes/"$RELEASE-$VERSION"
 
 [ -e /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg ] && asr restore -source /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg -target /Volumes/"$RELEASE-$VERSION" -noprompt -noverify -erase
@@ -57,9 +57,9 @@ fi
 
 hdiutil attach ./"$RELEASE-$VERSION".dmg -noverify -mountpoint /Volumes/"$RELEASE-$VERSION"
 	rm /Volumes/"$RELEASE-$VERSION"/System/Installation/Packages
-	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/Packages 				/Volumes/"$RELEASE-$VERSION"/System/Installation/
+	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/Packages 		/Volumes/"$RELEASE-$VERSION"/System/Installation/
 	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.chunklist 	/Volumes/"$RELEASE-$VERSION"
-	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg 		/Volumes/"$RELEASE-$VERSION"
+	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg 	/Volumes/"$RELEASE-$VERSION"
 
 set +x
 hdiutil detach /Volumes/"$RELEASE-$VERSION"
@@ -69,7 +69,7 @@ hdiutil detach /Volumes/"OSX-$VERSION-InstallESD"
 [ -e "./$RELEASE-$VERSION.iso" ] && rm ./"$DMG_NAME".dmg ./"$ESD_NAME".dmg 2>/dev/null
 
 [ ! -e "macOS-$VERSION-$RELEASE.iso" ] && mv "$RELEASE-$VERSION".iso "macOS-$VERSION-$RELEASE".iso
-[ -e "macOS-$VERSION-$RELEASE.iso" ] && set +x && echo '' && echo "MacOS ISO Success: $DIR_PATH/macOS-$VERSION-$RELEASE.iso" && echo ''
+[ -e "macOS-$VERSION-$RELEASE.iso" ] && echo '' && echo "MacOS ISO Success: $DIR_PATH/macOS-$VERSION-$RELEASE.iso" && echo ''
 read -p "Press enter to continue"
 
 exit 0
