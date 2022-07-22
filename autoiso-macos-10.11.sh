f3e86b713e491d1cee4ad288d3f9e9a45414441f
cloudcodenyc <98148819+cloudcodenyc@users.noreply.github.com>
2022-07-22 00:47:31 -0400

    Initial Commit
    Add Initial Scripts (v0.0.1)

---
+++ autoiso-macos-10.11.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+###############################################################################
+# MacOS-10.11-(ElCapitan): Automatically download and generate bootable ISO   #
+#                                                                             #
+# VERSION  = 0.0.1                                                            #
+# FILENAME = autoiso-macos-10.11.sh                                           #
+# LOCATION = https://github.com/cloudcodenyc/vmware-esxi-macosx               #
+# LICENSE  = BSD-3                                                            #
+#                                                                             #
+###############################################################################
+#                                                                             #
+# https://bootableinstaller.com/macos/#macos                                  #
+# https://gist.github.com/coolaj86/8c36d132250163011c83bad8284975ee           #
+# https://gist.github.com/wrossmck/20858f8393d3bb9158a1#file-makeosxiso-sh    #
+#                                                                             #
+###############################################################################
+	
+	VERSION="10.11.X"
+	RELEASE="ElCapitan"
+	APP_NAME="Install OS X El Capitan"
+	APP_PATH="/Applications/$APP_NAME"
+	DIR_PATH="$HOME/Desktop/OSX-ISO"
+	DMG_NAME="macOS-$VERSION-$RELEASE"
+	ESD_NAME="macOS-$VERSION-InstallESD"
+	ISO_FULL="$DIR_PATH/$DMG_NAME"
+	
+	APPLEURL=https://support.apple.com/en-us/HT211683
+	URL_1010=http://updates-http.cdn-apple.com/2019/cert/061-41343-20191023-02465f92-3ab5-4c92-bfe2-b725447a070d/InstallMacOSX.dmg
+	URL_1011=http://updates-http.cdn-apple.com/2019/cert/061-41424-20191024-218af9ec-cf50-4516-9011-228c78eda3d2/InstallMacOSX.dmg
+	URL_1012=http://updates-http.cdn-apple.com/2019/cert/061-39476-20191023-48f365f4-0015-4c41-9f44-39d3d2aca067/InstallOS.dmg
+	
+if [ ! -e "$DIR_PATH" ]; then mkdir "$DIR_PATH"; elif [ -e "$DIR_PATH" ]; then cd "$DIR_PATH"; else echo "ISO DIRECTORY ERROR"; fi
+	[ ! -e "$PWD/$DMG_NAME".dmg ] && curl -L --url "$URL_1011" -o "$DMG_NAME".dmg
+
+if [ -e "$DMG_NAME".dmg ]; then
+	hdiutil attach "$DMG_NAME".dmg -noverify -nobrowse -mountpoint "/Volumes/Install-OSX-$VERSION/"
+	pkgutil --expand /Volumes/"Install-OSX-$VERSION"/InstallMacOSX.pkg "./Install-OSX-$VERSION/"
+	hdiutil detach /Volumes/"Install-OSX-$VERSION" 2>/dev/null
+	[ -e /Volumes/"Install-OSX-$VERSION" ] && sudo hdiutil detach /Volumes/"Install-OSX-$VERSION" 2>/dev/null
+	mv ./"Install-OSX-$VERSION"/InstallMacOSX.pkg/InstallESD.dmg "$DIR_PATH/$ESD_NAME".dmg;
+fi
+
+[ -e "./Install-OSX-$VERSION/" ] && rm -rf "./Install-OSX-$VERSION/"
+	hdiutil attach "./$ESD_NAME.dmg" -noverify -nobrowse -mountpoint "/Volumes/OSX-$VERSION-InstallESD/"
+	# Get InstallESD.dmg Total Non-Empty Size ( Bytes )
+	ESDSIZE=$(hdiutil imageinfo ./"$ESD_NAME".dmg | grep --line-buffered -e 'Total Non-Empty Bytes:' | cut -d ' ' -f4)
+	EFISIZE=$(hdiutil imageinfo ./"$ESD_NAME".dmg | grep --line-buffered -e 'partition-name: EFI System Partition' -A7 | grep -e 'partition-length:' | cut -d ' ' -f2)
+	echo '' ; echo "$ESD_NAME.dmg" && echo "Filesize (bytes): $COUNT" ; echo ''
+
+[ ! -f "$RELEASE-$VERSION.dmg" ] && hdiutil create -o ./"$RELEASE-$VERSION".dmg -size "735	0m" -volname "$RELEASE-$VERSION" -layout SPUD -fs HFS+J
+	set -x && hdiutil attach ./"$RELEASE-$VERSION".dmg -noverify -mountpoint /Volumes/"$RELEASE-$VERSION"
+
+[ -e /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg ] && asr restore -source /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg -target /Volumes/"$RELEASE-$VERSION" -noprompt -noverify -erase
+	hdiutil detach /Volumes/"OS X Base System" || true
+	hdiutil detach /Volumes/'OS X Base System 1' 2>/dev/null ;
+	hdiutil detach /Volumes/"$RELEASE-$VERSION" 2>/dev/null ;
+
+hdiutil attach ./"$RELEASE-$VERSION".dmg -noverify -mountpoint /Volumes/"$RELEASE-$VERSION"
+	rm /Volumes/"$RELEASE-$VERSION"/System/Installation/Packages
+	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/Packages 				/Volumes/"$RELEASE-$VERSION"/System/Installation/
+	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.chunklist 	/Volumes/"$RELEASE-$VERSION"
+	cp -rp /Volumes/"OSX-$VERSION-InstallESD"/BaseSystem.dmg 		/Volumes/"$RELEASE-$VERSION"
+
+set +x
+hdiutil detach /Volumes/"$RELEASE-$VERSION"
+hdiutil detach /Volumes/"OSX-$VERSION-InstallESD"
+
+[ -e "./$RELEASE-$VERSION.dmg" ] && mv ./"$RELEASE-$VERSION".dmg ./"$RELEASE-$VERSION".iso
+[ -e "./$RELEASE-$VERSION.iso" ] && rm ./"$DMG_NAME".dmg ./"$ESD_NAME".dmg 2>/dev/null
+
+[ ! -e "macOS-$VERSION-$RELEASE.iso" ] && mv "$RELEASE-$VERSION".iso "macOS-$VERSION-$RELEASE".iso
+[ -e "macOS-$VERSION-$RELEASE.iso" ] && set +x && echo '' && echo "MacOS ISO Success: $DIR_PATH/macOS-$VERSION-$RELEASE.iso" && echo ''
+read -p "Press enter to continue"
+
+exit 0
---
+++ autovmx-darwin-tools.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+###############################################################################
+# Enable native Guest Tools for MacOS X ("darwin") on VMware ESXi v6/v7       #
+#                                                                             #                                                        #
+# VERSION  = 0.0.1                                                            #
+# FILENAME = autovmx-darwin-tools.sh                                          #
+# LOCATION = https://github.com/cloudcodenyc/vmware-esxi-macosx               #
+# LICENSE  = BSD-3    
+#                                                                             #
+###############################################################################
+#
+# https://docs.vmware.com/en/VMware-Tools
+# https://kb.vmware.com/s/article/2129825 (Installing and upgrading the latest version of VMware Tools)
+# https://customerconnect.vmware.com/downloads/info/slug/datacenter_cloud_infrastructure/vmware_tools/12_x
+# https://docs.vmware.com/en/VMware-Tools/12.0.0/com.vmware.vsphere.vmwaretools.doc/GUID-D8892B15-73A5-4FCE-AB7D-56C2C90BD951.html
+
+	VMX_URL="http://softwareupdate.vmware.com/cds/vmw-desktop/fusion/"
+	VMX_V11="http://softwareupdate.vmware.com/cds/vmw-desktop/fusion/11.1.0/13668589/packages/com.vmware.fusion.tools.darwin.zip.tar"
+	VMX_V10="http://softwareupdate.vmware.com/cds/vmw-desktop/fusion/10.1.6/12989998/packages/com.vmware.fusion.tools.darwin.zip.tar"
+	VMX_ARM="http://softwareupdate.vmware.com/cds/vmw-desktop/fusion/12.2.3/19436697/arm64/core/com.vmware.fusion.zip.tar"
+	VMX_X86="http://softwareupdate.vmware.com/cds/vmw-desktop/fusion/12.2.3/19436697/x86/core/com.vmware.fusion.zip.tar"
+
+	ISOREPO=/productLocker/vmtools
+	VMXREPO=/locker/packages/vmtoolsRepo/vmtools
+	VMXUSER=/vmfs/volumes/datastore1/.vmtools-osx
+	
+[ ! -e /vmfs/volumes/datastore1 ] && echo "ERROR: CANNOT FIND PRIMARY DATASTORE" && read -p "Press enter to continue" && exit 1
+[ ! -e "$VMXUSER" ] && mkdir "$VMXUSER" ; mkdir $VMXUSER/darwin-active $VMXUSER/darwin-stable $VMXUSER/darwin-fusion 2>/dev/null
+
+	# Create symbolic links in user accessible storage that redirects to the "darwin-active" directory
+	[ ! -e "$VMXUSER/darwin.iso" ] && cd "$VMXUSER" && ln -s darwin-active/darwin.iso ./darwin.iso
+	[ ! -e "$VMXUSER/darwin.iso.sha" ] && cd "$VMXUSER" && ln -s darwin-active/darwin.iso.sha ./darwin.iso.sha
+	[ ! -e "$VMXUSER/darwin.iso.sig" ] && cd "$VMXUSER" && ln -s darwin-active/darwin.iso.sig ./darwin.iso.sig
+
+[ ! -e $VMXUSER/darwin-fusion/darwin-fusion-11.1.0.zip.tar ] && wget "$VMX_V11" -O $VMXUSER/darwin-fusion/darwin-fusion-11.1.0.zip.tar
+	[ -f $VMXUSER/darwin-fusion/darwin-fusion-11.1.0.zip.tar ] && TOOLS_V11="$VMXUSER/darwin-fusion/darwin-fusion-11.1.0"
+
+[ ! -e $VMXUSER/darwin-fusion/darwin-fusion-10.1.6.zip.tar ] && wget "$VMX_V10" -O $VMXUSER/darwin-fusion/darwin-fusion-10.1.6.zip.tar
+	[ -f $VMXUSER/darwin-fusion/darwin-fusion-10.1.6.zip.tar ] && TOOLS_V10="$VMXUSER/darwin-fusion/darwin-fusion-10.1.6"
+		
+[ ! -e "$TOOLS_V11" ] && mkdir "$TOOLS_V11" && tar xv -C "$TOOLS_V11"/ -f "$TOOLS_V11".zip.tar && unzip -j "$TOOLS_V11/com.vmware.fusion.tools.darwin.zip" -d "$TOOLS_V11"/
+	[ -f "$TOOLS_V11/darwin.iso" ] && rm -f "$TOOLS_V11/com.vmware.fusion.tools.darwin.zip" "$TOOLS_V11/descriptor.xml" 2>/dev/null
+		[ ! -e "$VMXUSER/darwin-active/darwin.iso" ]	&& cd "$TOOLS_V11" && cp -pu *darwin* $VMXUSER/darwin-active/
+	
+[ ! -e "$TOOLS_V10" ] && mkdir "$TOOLS_V10" && tar xv -C "$TOOLS_V10"/ -f "$TOOLS_V10".zip.tar && unzip -j "$TOOLS_V10/com.vmware.fusion.tools.darwin.zip" -d "$TOOLS_V10"/
+	[ -f "$TOOLS_V10/darwin.iso" ] && rm -f "$TOOLS_V10/com.vmware.fusion.tools.darwin.zip" "$TOOLS_V10/descriptor.xml" 2>/dev/null
+		[ ! -e "$VMXUSER/darwin-active/darwin.iso" ]	&& cd "$TOOLS_V10" && cp -pu *darwin* $VMXUSER/darwin-active/	
+	
+	# Create symbolic links in "vmtoolsRepo" that redirect to VMware "darwin" Guest Tools ISO
+	[ ! -e "$VMXREPO/darwin.iso" ] && cd "$VMXREPO" && ln -s "$VMXUSER"/darwin.iso ./darwin.iso
+	[ ! -e "$VMXREPO/darwin.iso.sha" ] && cd "$VMXREPO" && ln -s "$VMXUSER"/darwin.iso.sha ./darwin.iso.sha
+	[ ! -e "$VMXREPO/darwin.iso.sig" ] && cd "$VMXREPO" && ln -s "$VMXUSER"/darwin.iso.sig ./darwin.iso.sig
+	
+cd "$VMXUSER"/ ; exit 0
